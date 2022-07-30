73dd4e05ffe91ff1bbb1c3ba94fa9e7bf3df46a4
Aldo Monroy <aldomonroyg@gmail.com>
2022-07-29 21:41:04 -0500

    second commit

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -7,12 +7,16 @@ function selectPetPayer(){
     let inputCapipepo = document.getElementById('Capipepo')
     let inputRatigueya = document.getElementById('Ratigueya')
     let inputLangostelvis = document.getElementById('Langostelvis')
+    let spanPetPlayer = document.getElementById('pet-player')
 
     if(inputCapipepo.checked){
+        spanPetPlayer.innerHTML = 'Capipepo'
         alert("Select Capipepo!")
     } else if (inputRatigueya.checked){
+        spanPetPlayer.innerHTML = 'Ratigueya'
         alert("Select Ratigueya!")
     } else if (inputLangostelvis.checked){
+        spanPetPlayer.innerHTML = 'Langostelvis'
         alert("Select Langostelvis!")
     } else(alert ("Select a pet")
     )
